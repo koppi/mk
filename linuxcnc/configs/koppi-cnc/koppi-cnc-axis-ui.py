22f4432228048a1fa468d1605cff7746e6129ab4
Jakob Flierl <jakob.flierl@gmail.com>
2015-05-16 00:36:40 +0200

    misc fixes

--- linuxcnc/configs/koppi-cnc/koppi-cnc-axis-ui.py
+++ linuxcnc/configs/koppi-cnc/koppi-cnc-axis-ui.py
@@ -2,12 +2,30 @@ from subprocess import call
 
 root_window.tk.call(pane_bottom+".t.text","configure"
 ,"-background","black")
+<<<<<<< HEAD
+
+=======
+>>>>>>> db2527241f251c7c082fe5c6a38c069c59a166bd
 root_window.tk.call(pane_bottom+".t.text","configure"
 ,"-foreground","white")
 
 root_window.bind("<Control-q>", "destroy .")
 help2.append(("Control-Q", "Quit"))
 
+<<<<<<< HEAD
+######################################################################
+# the following is experimental and does not work reliably
+######################################################################
+
+# disable estop at startup  ( sometimes not working )
+call(["halcmd", "setp", "halui.estop.reset", "1"])
+call(["halcmd", "setp", "halui.estop.reset", "0"])
+
+# enable machine at startup ( sometimes not working )
+call(["halcmd", "setp", "halui.machine.on", "1"])
+
+# home all axes at startup  ( not working at all )
+=======
 # disable estop at startup
 call(["halcmd", "setp", "halui.estop.reset", "1"])
 call(["halcmd", "setp", "halui.estop.reset", "0"])
@@ -16,6 +34,7 @@ call(["halcmd", "setp", "halui.estop.reset", "0"])
 call(["halcmd", "setp", "halui.machine.on", "1"])
 
 # home all axes at startup ( not working )
+>>>>>>> db2527241f251c7c082fe5c6a38c069c59a166bd
 #ensure_mode(linuxcnc.MODE_MANUAL)
 #c.wait_complete()
 #commands.home_all_axes()
