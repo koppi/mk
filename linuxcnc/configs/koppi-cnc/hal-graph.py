af914e6dcf309160ad8571d493880a1c83232191
Jakob Flierl <jakob.flierl@gmail.com>
2015-06-02 02:18:24 +0200

    rename hal-graph-mk.py to hal-graph.py

--- linuxcnc/configs/koppi-cnc/hal-graph.py
+++ linuxcnc/configs/koppi-cnc/hal-graph.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python2
 #
-# Interactive HAL Graph
+# HAL Graph
 #
-# Copyright 2015 Jakob Flierl
+# Copyright
+# - 2015 Jakob Flierl <jakob.flierl@gmail.com>
 #
-# based on https://github.com/MachineryScience/Rockhopper/blob/master/MakeHALGraph.py
+# inspired by https://github.com/MachineryScience/Rockhopper/blob/master/MakeHALGraph.py
 #
-# this version differs to the above version in the following points:
-#
-# * it auto-refreshes the HAL data every five seconds
-#
-# * opens an interactive window (using a slightly modified version of xdot.py)
+# this version differs to MakeHALGraph.py as follows:
 #
 # * uses the pydot lib instead of the pygraphviz lib
 #   (pygraphviz export seems to export dot files in a non-deterministic manner)
+#
+# * gtk gui (- using a slightly modified version of xdot.py)
+#
+# * gui refreshes the HAL data every [interval] seconds
 
 import os
 import sys
 import time
 import math
-import linuxcnc
 import hal
 import subprocess
 import pydot
@@ -31,92 +31,85 @@ import xdot
 
 import warnings
 
+#from linuxcnc   import hal
+from machinekit import hal
+
 class HALAnalyzer( object ):
     def __init__(self ):
         self.Graph = pydot.Dot(graph_type='digraph', name="Hal graph",
-                               rankdir='LR', splines='spline',
-                               overlap='false', start='regular',
-                               forcelabels='true')
+                               rankdir='LR',
+                               splines='spline',
+                               overlap='false',
+                               start='regular',
+                               #pad=".05",
+                               #ranksep="0.75",
+                               #nodesep="0.05"
+        )
+
+    def groupname(self, pinname): # TODO check if this works with every pin name
+        pstr = pinname.split('.')
+        pin_group_name = pstr[0]
+        try:
+            if (len(pstr) > 2) and (int(pstr[1] > -1)):
+                pin_group_name = pstr[0] + '.' + pstr[1]
+        except Exception, err:
+            pass
+        return pin_group_name
+
+    def dotname(self, name):
+        return name.replace(".","_").replace("-", "_")
                 
-    def parse_comps( self ):
-        p = subprocess.Popen(['halcmd', '-s', 'show', 'comp'],
-                             stderr=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=(1024*64))
-        
-        raw = p.communicate()[0].split( '\n' )
-        components = [ filter( lambda a: a != '', [x.strip() for x in line(' ')] ) for line in raw ]
-        
-        self.component_dict = {}
-        
-        for c in components:
-            if len(c) == 4:
-                c.append( c[3] )
-                c[3] = ''
-            if ( c[2].find( 'halcmd' ) != 0 ):
-                #self.Graph.add_node( c[2] )
-                self.component_dict[ c[2] ] = c
-           
-    def parse_pins( self ):
-        p = subprocess.Popen(['halcmd', '-s', 'show', 'pin'],
-                             stderr=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             bufsize=(1024*64))
-        
-        raw = p.communicate()[0].split( '\n' )
-        pins = [ filter( lambda a: a != '', [x.strip() for x in line.split(' ')] ) for line in raw ]
+    def parse( self ):
         self.pin_group_dict = {}
         self.sig_dict = {}
-        for p in pins:
-            if len(p) > 5:
+        for pin in hal.pins():
+            p = hal.Pin(pin)
+            if p.signal:
                 # if there is a signal listed on this pin, make sure
                 # that signal is in our signal dictionary
-                if ( p[6] in self.sig_dict ):
-                    self.sig_dict[ p[6] ].append( p )
+                if ( p.signal.name in self.sig_dict ):
+                    self.sig_dict[ p.signal.name ].append( p )
                 else:
-                    self.sig_dict[ p[6] ] = [ p ]
-                    self.Graph.add_node(pydot.Node(p[6],
-                                                   label=p[6] + " (" + p[3] + ")",
-                                                   shape='box',
-                                                   style='dotted',
-                                                   outputMode='nodesfirst'))
-
-                pstr = p[4].split('.')
-                pin_group_name = pstr[0]
-                try:
-                    if (len(pstr) > 2) and (int(pstr[1] > -1)):
-                        pin_group_name = pstr[0] + '.' + pstr[1]
-                except Exception, err:
-                    pass
- 
-                if pin_group_name in self.pin_group_dict:
-                    self.pin_group_dict[ pin_group_name ].append( p )
+                    self.sig_dict[ p.signal.name ] = [ p ]
+                    tmp = '<<table border="0" cellspacing="0">\n  <tr><td port="signame" bgcolor="lightgray" border="1">' + p.signame + '</td></tr>' + \
+                          '\n  <tr><td port="signame" border="1">(' + str(p.get()) + ')</td></tr></table>>'
+                    self.Graph.add_node(pydot.Node(p.signame,
+                                                   label=tmp,
+                                                   shape='none'
+                                                   ))
+                n = self.groupname(p.name)
+            
+                if n in self.pin_group_dict:
+                    self.pin_group_dict[ n ].append( p )
                 else:
-                    self.pin_group_dict[ pin_group_name ] = [ p ]
+                    self.pin_group_dict[ n ] = [ p ]
 
         # Add all the pins into their sub-graphs
         for pin_g_name, pin_g_val_array in self.pin_group_dict.iteritems():
-            subg = pydot.Cluster('cluster_' + pin_g_name.replace(".","_").replace("-", "_"),
-                                 label=pin_g_name, style='rounded' )
-            
-            #print 'GROUP: ', pin_g_name
-            for pin_g_val in pin_g_val_array:
-                #print '      ', pin_g_val[4]
-                subg.add_node(pydot.Node(pin_g_val[4], label=pin_g_val[4],
-                                         shape='box', style='filled', color='lightgrey' ))
-            self.Graph.add_subgraph(subg)
+            tmp = '<<table border="0" cellspacing="0">\n  <tr><td port="title" border="1" bgcolor="#FFD75E">' + pin_g_name + '</td></tr>\n'
+            for pin in pin_g_val_array:
+                label =pin.name.split('.')[-1]
+                name = self.dotname(pin.name.split('.')[-1])
+                tmp += '  <tr><td port="'+ name + '" border="1">' + label + '</td></tr>\n'
+            tmp += "</table>>"
+            self.Graph.add_node(pydot.Node(self.dotname(pin_g_name), label=tmp, shape="none"))
 
         # Add all the edges to and from signals
-        for sig_name, pin_g_val_array in self.sig_dict.iteritems():
-            for pin_g_val in pin_g_val_array:
-                if (pin_g_val[5] == '==>'):
-                    self.Graph.add_edge(pydot.Edge( pin_g_val[4], sig_name, color='#0000E0' ))
+        for sig in hal.signals():
+            s = hal.Signal(sig)
+            for pin in s.pins():
+                n1 = self.dotname(self.groupname(pin.name)) + ":" + self.dotname(pin.name.split(".")[-1])
+                n2 = s.name
+                if (pin.name == s.writername):
+                    self.Graph.add_edge(pydot.Edge( n1, n2, arrowhead="none", penwidth = 2, splines="ortho" ))
                 else:
-                    self.Graph.add_edge(pydot.Edge( sig_name, pin_g_val[4] ))
-
-    def write_svg( self, filename ):
+                    self.Graph.add_edge(pydot.Edge( n2, n1, penwidth = 2, splines="ortho" ))
+        
+    def write_svg( self, dot_filter, filename ):
         try:
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore",category=DeprecationWarning)
-                output = self.Graph.create_svg(prog='dot' )
+                output = self.Graph.create_svg(prog=dot_filter )
                 f = open(filename, "w")
                 f.write(output)
                 f.close()
@@ -128,24 +121,32 @@ class HALAnalyzer( object ):
 
 class HALGraphWindow(xdot.DotWindow):
 
-    def __init__(self):
+    def __init__(self, dot_filter):
         xdot.DotWindow.__init__(self)
         self.base_title = "HAL Graph"
-        self.set_filter("dot") # 'dot', 'neato', 'twopi', 'circo', 'fdp'
+        self.set_filter(dot_filter)
         self.widget.connect('clicked', self.on_url_clicked)
         self.analyzer = None
         self.reload()
         self.widget.zoom_to_fit()
+        self.interval = 0
 
+    def timer_interval(self, interval):
+        self.interval = interval
+
+    def enable_timer(self):
+        if (self.interval > 0):
+            self.timer = gtk.timeout_add(self.interval, self.timer_tick)
+        
     def reload(self):
         self.analyzer = HALAnalyzer()
-        self.analyzer.parse_pins()
+        self.analyzer.parse()
         self.set_dotcode(self.analyzer.to_string())
         self.textentry_changed(self.widget, self.textentry)
 
         self.queue_draw()
 
-    def on_reload(self, action):
+    def on_reload(self, action): # reload button
         self.reload()
 
     def on_url_clicked(self, widget, url, event):
@@ -157,22 +158,26 @@ class HALGraphWindow(xdot.DotWindow):
         dialog.run()
         return True
 
-    def timer(self):
+    def timer_tick(self):
+        gtk.timeout_remove(self.timer)
         self.reload()
+        self.enable_timer()
         return True
 
 def main():
+    dot_filter = "dot" # 'neato', 'twopi', ..
     if len(sys.argv) == 1:
-        w = HALGraphWindow()
+        w = HALGraphWindow(dot_filter)
         w.connect('destroy', gtk.main_quit)
-        gtk.timeout_add(5000, w.timer)
+        w.timer_interval(5000)
+        w.enable_timer()
         gtk.main()
     elif len(sys.argv) == 2:
         a = HALAnalyzer()
-        a.parse_pins()
-        a.write_svg(sys.argv[1])
+        a.parse()
+        a.write_svg(dot_filter, sys.argv[1])
     else:
-        print "usage: hal-graph.py (hal-graph.svg)"
+        print "usage: hal-graph.py [hal-graph.svg]"
 
 if __name__ == '__main__':
     main()
