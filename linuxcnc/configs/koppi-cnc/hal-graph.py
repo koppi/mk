164daa77b4003285995cdcb42478967f17546b6a
Jakob Flierl <jakob.flierl@gmail.com>
2015-06-02 03:50:08 +0200

    Add license

--- linuxcnc/configs/koppi-cnc/hal-graph.py
+++ linuxcnc/configs/koppi-cnc/hal-graph.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python2
 #
+# Copyright (C) 2015 Jakob Flierl <jakob.flierl@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
 # HAL Graph
 #
-# Copyright
-# - 2015 Jakob Flierl <jakob.flierl@gmail.com>
+#  inspired by https://github.com/MachineryScience/Rockhopper/blob/master/MakeHALGraph.py
 #
-# inspired by https://github.com/MachineryScience/Rockhopper/blob/master/MakeHALGraph.py
+#  this version differs to MakeHALGraph.py as follows:
 #
-# this version differs to MakeHALGraph.py as follows:
+#  * uses the pydot lib instead of the pygraphviz lib
+#    (pygraphviz export seems to export dot files in a non-deterministic manner)
 #
-# * uses the pydot lib instead of the pygraphviz lib
-#   (pygraphviz export seems to export dot files in a non-deterministic manner)
+#  * gtk gui (- using a slightly modified version of xdot.py)
 #
-# * gtk gui (- using a slightly modified version of xdot.py)
-#
-# * gui refreshes the HAL data every [interval] seconds
+#  * gui refreshes the HAL data every [interval] seconds
 
 import os
 import sys
